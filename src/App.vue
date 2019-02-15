0a3800b745476eb5c892f11cba666e7fc5df1399
Antoine Dcr <antoine.dcr@outlook.fr>
2019-02-15 00:41:12 -0800

    Add configuration files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
+        <span>Vuetify {{productionMode}}</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -32,8 +32,13 @@
     },
   })
   export default class App extends Vue {
+    public appTitle: string = 'My App';
     constructor() {
       super();
+    }
+
+    public mounted() {
+      this.appTitle = process.env.VUE_APP_TITLE;
     }
   }
 </script>
