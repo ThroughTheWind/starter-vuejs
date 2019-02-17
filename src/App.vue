5d11fdeabe71647cfe985973073b1b2d5e948674
ThroughTheWind <antoine.dcr@outlook.fr>
2019-02-17 11:46:07 +0100

    Add package.lock file

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify {{productionMode}}</span>
+        <span>Vuetify {{appTitle}}</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
