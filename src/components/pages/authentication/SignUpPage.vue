89069b9bb432898baba8b5ca867b35449e99a38b
ThroughTheWind <antoine.dcr@outlook.fr>
2019-02-23 16:07:59 +0100

    Working on authentication pages

--- src/components/pages/authentication/SignUpPage.vue
+++ src/components/pages/authentication/SignUpPage.vue
@@ -6,16 +6,17 @@
           <v-toolbar dark color="primary">
             <v-toolbar-title>Create new account</v-toolbar-title>
           </v-toolbar>
+          <!--
           <v-card-text>
             <v-form>
               <v-text-field name="login" v-model="email" label="E-mail" type="email"></v-text-field>
               <v-text-field id="password" v-model="password" name="password" label="Password" type="password"></v-text-field>
               <v-text-field id="password" v-model="confirmPassword" name="password" label="Confirm password" type="password"></v-text-field>
             </v-form>
-          </v-card-text>
-                <!--
+          </v-card-text>-->
+                
           <v-stepper v-model="stepper">
-            <v-stepper-header>
+            <v-stepper-header class="elevation-0">
               <v-stepper-step :complete="stepper > 1" step="1">Account infos</v-stepper-step>
 
               <v-divider></v-divider>
@@ -27,10 +28,10 @@
               <v-stepper-step step="3">Additional infos</v-stepper-step>
             </v-stepper-header>
             <v-stepper-items>
-              <v-stepper-content step="1">
+              <v-stepper-content step="1" class="elevation-0">
                 <v-card-text>
                   <v-form>
-                    <v-text-field name="login" v-model="email" label="E-mail" type="text"></v-text-field>
+                    <v-text-field name="login" v-model="email" label="E-mail" type="e-mail"></v-text-field>
                     <v-text-field id="password" v-model="password" name="password" label="Password" type="password"></v-text-field>
                     <v-text-field id="password" v-model="confirmPassword" name="password" label="Confirm password" type="password"></v-text-field>
                   </v-form>
@@ -39,51 +40,79 @@
                   color="primary"
                   @click="stepper = 2"
                 >
-                  Continue
+                  Next step
                 </v-btn>
-
-                <v-btn flat href="home">Cancel</v-btn>
+                <v-btn flat href="home">Quick creation</v-btn>
+                <v-btn href="sign-in" color="accent" outline>Already has an account ?</v-btn>
               </v-stepper-content>
 
-              <v-stepper-content step="2">
-                <v-card
-                  class="mb-5"
-                  color="grey lighten-1"
-                  height="200px"
-                ></v-card>
+              <v-stepper-content step="2" class="elevation-0">
+                <v-card-text>
+                  <v-form>
+                    <v-text-field name="firstname" v-model="email" label="First name" type="text"></v-text-field>
+                    <v-text-field id="lastname" v-model="password" name="lastname" label="Last name" type="text"></v-text-field>
+                    <v-menu
+                      ref="menu"
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      :return-value.sync="date"
+                      lazy
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      min-width="290px"
+                    >
+                      <v-text-field
+                        slot="activator"
+                        v-model="date"
+                        label="Picker in menu"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                      ></v-text-field>
+                      <v-date-picker v-model="date" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                        <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                  </v-form>
+                </v-card-text>
 
                 <v-btn
                   color="primary"
-                  @click="e1 = 3"
+                  @click="stepper = 3"
                 >
                   Continue
                 </v-btn>
 
                 <v-btn flat @click="stepper = 1">Return</v-btn>
+                <v-btn href="sign-in" color="accent" outline>Already has an account ?</v-btn>
               </v-stepper-content>
 
-              <v-stepper-content step="3">
+              <v-stepper-content step="3" class="elevation-0">
                 <v-card
                   class="mb-5"
                   color="grey lighten-1"
                   height="200px"
-                ></v-card>
+                >
+                  Additionnal informations
+                </v-card>
 
                 <v-btn
                   color="primary"
-                  @click="e1 = 1"
+                  @click="stepper = 1"
                 >
-                  Continue
+                  Create
                 </v-btn>
-
-                <v-btn flat>Cancel</v-btn>
+                <v-btn flat @click="stepper = 1">Cancel</v-btn>
+                <v-btn href="sign-in" color="accent" outline>Already has an account ?</v-btn>
               </v-stepper-content>
             </v-stepper-items>
-          </v-stepper>-->
+          </v-stepper>
           
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn href="sign-in" color="accent" outline>Already has an account ?</v-btn>
             <v-btn href="home" color="primary" :disabled="true">Create</v-btn>
           </v-card-actions>
         </v-card>
@@ -106,6 +135,8 @@
     public password: string = '';
     public confirmPassword: string = '';
     public stepper: number = 1;
+    public date: any = new Date().toISOString().substr(0, 10);
+    public menu: boolean = false;
     constructor() {
       super();
     }
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
+import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
+    primary: colors.green,
     secondary: '#424242',
     accent: '#82B1FF',
     error: '#FF5252',
